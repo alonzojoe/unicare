b06dabcc3d9ed9a0389bd3f19243b97cb4772947
alonzojoe <alonzojoenell2k15@gmail.com>
2025-12-18 10:48:53 +0800

    create UI UX for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Navbar";
+
 function App() {
   return (
-    <main className="text-lg text-palette-neutral-600">
-      <h2>Test</h2>
+    <main className="text-lg text-palette-neutral-600 antialiased">
+      <Header />
     </main>
   );
 }
